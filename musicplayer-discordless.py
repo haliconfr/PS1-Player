95bae7a531473436cfc6353bc90a816ae84abd0c
Max Tearney <88261993+maxtearney@users.noreply.github.com>
2023-07-25 20:05:11 +0800

    Create musicplayer-discordless.py
    probably broken again coz i used notepad but ill fix it later if it is

---
+++ musicplayer-discordless.py
@@ -0,0 +1,164 @@
+import os
+import json
+import spotipy
+import math
+import time
+from ctypes import windll, byref, create_unicode_buffer, create_string_buffer
+from threading import Thread
+from time import strftime
+from time import gmtime
+from spotipy.oauth2 import SpotifyOAuth
+from tkinter import *
+from PIL import Image
+from PIL import ImageTk
+from tkextrafont import Font
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+tk = Tk()
+tk.title("PS1 - Spotify Player")
+tk.geometry("821x626")
+tk.wm_iconbitmap(bitmap = resource_path(r"images\icon.ico"))
+mainFont = "Gamestation Condensed"
+paused = False
+tiles = []
+SpotifyObject = 0
+progress = 0
+bg = ImageTk.PhotoImage(Image.open(resource_path(r"images\\background.png")))
+play = ImageTk.PhotoImage(Image.open(resource_path(r"images\\play.png")))
+pause = ImageTk.PhotoImage(Image.open(resource_path(r"images\\pause.png")))
+skiplayout = ImageTk.PhotoImage(Image.open(resource_path(r"images\\skiplayout.png")))
+forward = ImageTk.PhotoImage(Image.open(resource_path(r"images\\forward.png")))
+back = ImageTk.PhotoImage(Image.open(resource_path(r"images\\back.png")))
+timer2 = ImageTk.PhotoImage(Image.open(resource_path(r"images\\timer2.png")))
+Label(tk, image=bg).place(x=0, y=0, relwidth=1, relheight=1)
+Label(tk, image=skiplayout, borderwidth=0).place(x=193, y=498)
+Label(tk, image=timer2, borderwidth=0).place(x=68, y=53)
+secText = Label(tk, bg="#351189", text="s", fg="white", font=(mainFont, 25))
+minText = Label(tk, bg="#351189", text="m", fg="white", font=(mainFont, 25))
+secText.place(x=221, y=64)
+minText.place(x=105, y=64)
+tk.resizable(False, False)
+
+def quitHandler():
+    global tk
+    tk.destroy()
+
+def initTrackNum(data):
+    timesDone = 0
+    xValue = 321
+    yValue = 53
+    global tiles
+    artist = data["item"]["artists"][0]["name"]
+    name = data["item"]["name"]
+    tk.title("PS1: " + str(artist) + " - " + str(name))
+    if data["item"]["is_local"] == False:
+        tracks = data["item"]["album"]["total_tracks"]
+        size = len(tiles)
+        for i in range(size):
+            tiles[i].destroy()
+        for i in range(tracks):
+            xValue = xValue + 62
+            numTile = ImageTk.PhotoImage(Image.open(resource_path("images\\numbertile.png")))
+            numberTile = Label(tk, text=str(i + 1), font=(mainFont, 20), bg = "black", fg = "white", image=numTile, compound = "center", borderwidth=0)
+            numberTile.place(x=xValue, y=yValue)
+            tiles.append(numberTile)
+            timesDone += 1
+            if timesDone == 6:
+                yValue = yValue + 63
+                xValue = 321
+                timesDone = 0
+    else:
+        for i in range(size):
+            tiles[i].destroy()
+        numTile = ImageTk.PhotoImage(Image.open(resource_path("images\\numbertile.png")))
+        numberTile = Label(tk, text=str(1), font=(mainFont, 20), bg = "black", fg = "white", image=numTile, compound = "center", borderwidth=0).place(x=xValue + 62, y=yValue)
+
+def SpotifySetup():
+    global spotifyObject
+    username = ""
+    clientID = "CLIENTID"
+    clientSecret = "CLIENTSECRET"
+    scope = "user-read-currently-playing user-modify-playback-state"
+    redirectURI = "https://example.com/callback"
+    sp = spotipy.Spotify()
+    token = spotipy.util.prompt_for_user_token(username,
+                           scope,
+                           client_id = "CLIENTID",
+                           client_secret = "CLIENTSECRET",
+                           redirect_uri="https://example.com/callback")
+    spotifyObject = spotipy.Spotify(auth=token)
+    user = spotifyObject.current_user()
+    initTrackNum(spotifyObject.current_user_playing_track())
+    
+SpotifySetup()
+
+def PlayMusic():
+    global paused
+    if(paused):
+        paused = False
+        tk.after(10, TrackTime)
+        spotifyObject.start_playback()
+    
+def PauseMusic():
+    global paused
+    if(paused == False):
+        paused = True
+        spotifyObject.pause_playback()
+
+def SkipTrack():
+    spotifyObject.next_track()
+
+def BackTrack():
+    spotifyObject.previous_track()
+        
+def TrackTime(TrackInfo):
+    if(paused == False):
+        global spotifyObject
+        global secText
+        global minText
+        global minutes
+        global seconds
+        if(TrackInfo != null):
+            progress = int(TrackInfo["progress_ms"])
+        else:
+            progress = progress + 1000
+        seconds = math.trunc((progress/1000)%60)
+        minutes = math.trunc((progress/(1000*60))%60)
+        secText.config(text = math.trunc(seconds))
+        minText.config(text = math.trunc(minutes))
+    time.sleep(1)
+    Thread(target = TrackTime).start()
+
+def updateProgram():
+    data = spotifyObject.current_user_playing_track()
+    if(data["progress_ms"]/1000 < 2):
+        updateable = True
+    else:
+        updateable = False
+    if(updateable):
+        initTrackNum(data)
+    time.sleep(0.1)
+    Thread(target = updateProgram).start()
+
+def TrackTimeBump():
+    TrackInfo = spotifyObject.current_user_playing_track()
+    TrackTime(TrackInfo)
+    time.sleep(5)
+    Thread(target = TrackTime(TrackInfo)).start()
+
+tk.protocol('WM_DELETE_WINDOW', quitHandler)
+Thread(target = TrackTimeBump).start()
+Thread(target = updateProgram).start()
+Button(tk, image=play, command=PlayMusic, highlightthickness=0, borderwidth=0).place(x=193, y=514)
+Button(tk, image=pause, command=PauseMusic, highlightthickness=0, borderwidth=0).place(x=256, y=512)
+Button(tk, image=forward, command=SkipTrack, highlightthickness=0, borderwidth=0).place(x=442, y=511)
+Button(tk, image=back, command=BackTrack, highlightthickness=0, borderwidth=0).place(x=378, y=513)
+
+tk.mainloop()
